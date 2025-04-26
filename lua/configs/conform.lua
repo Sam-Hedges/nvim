e68070314029d670db6e6d39453a242e231abe6c
Sam Hedges <sam.hedges.sh@gmail.com>
2025-04-26 01:05:39 +0100

    Update conform.lua

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -1,6 +1,9 @@
+local conform = require "conform"
+
 local options = {
     formatters_by_ft = {
         lua = { "stylua" },
+        c = { "clang-format" },
         cpp = { "clang-format" },
         -- css = { "prettier" },
         -- html = { "prettier" },
@@ -12,5 +15,15 @@ local options = {
         lsp_fallback = true,
     },
 }
+
+conform.setup(options)
+
+-- Automatically format on save for any buffer that supports formatting
+vim.api.nvim_create_autocmd("BufWritePre", {
+    pattern = "*",
+    callback = function(args)
+        conform.format { bufnr = args.buf }
+    end,
+})
 
 return options
