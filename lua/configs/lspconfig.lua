c1923861e7b4120a34337eca97639a5384de66bb
Sam Hedges <45243156+Sam-Hedges@users.noreply.github.com>
2025-05-11 13:05:51 +0100

    Update lspconfig.lua
    Enable Semantic Tokens

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -3,36 +3,38 @@ require("nvchad.configs.lspconfig").defaults()
 -- local servers = { "html", "cssls", "lua_ls", "clangd" }
 -- vim.lsp.enable(servers)
 
--- lsps with default config
--- for _, lsp in ipairs(servers) do
---     lspconfig[lsp].setup {
---         on_attach = nvlsp.on_attach,
---         on_init = nvlsp.on_init,
---         capabilities = nvlsp.capabilities,
---     }
--- end
-
-local on_init = require("nvchad.configs.lspconfig").on_init
-local on_attach = require("nvchad.configs.lspconfig").on_attach
+local on_attach_default = require("nvchad.configs.lspconfig").on_attach
 local capabilities = require("nvchad.configs.lspconfig").capabilities
 local lspconfig = require "lspconfig"
 
--- C and CPP setup
-vim.lsp.enable "clangd"
--- lspconfig.clangd.setup {
---     cmd = {
---         "clangd",
---         "--fallback-style={BasedOnStyle: llvm, IndentWidth: 4}",
---     },
---     on_attach = function(client, bufnr)
---         client.server_capabilities.documentFormattingProvider = false
---         client.server_capabilities.documentRangeFormattingProvider = false
---         client.server_capabilities.semanticTokensProvider = true
---         on_attach(client, bufnr)
---     end,
---     on_init = on_init,
---     capabilities = capabilities,
--- }
+lspconfig.clangd.setup {
+  cmd = {
+    "clangd",
+    "--fallback-style={BasedOnStyle: llvm, IndentWidth: 4}",
+  },
+  on_attach = function(client, bufnr)
+    client.server_capabilities.documentFormattingProvider = false
+    client.server_capabilities.documentRangeFormattingProvider = false
+
+    -- Explicitly start semantic tokens
+    if client.server_capabilities.semanticTokensProvider then
+      vim.lsp.semantic_tokens.start(bufnr, client.id)
+    end
+
+    on_attach_default(client, bufnr)
+  end,
+
+  -- LIGMA BALLS NVCHAD WHY TF DO YOU NOT MENTION THIS IN THE DOCS
+  -- I WASTED 10 YEARS OF MY LIFE TRYING TO FIGURE OUT WHY SEMANTIC
+  -- TOKENS WERE DISABLED
+  
+  -- Do NOT import nvchad's `on_init`, it disables semantic tokens
+  on_init = function() end,
+
+  capabilities = capabilities,
+}
+
+local on_init = require("nvchad.configs.lspconfig").on_init
 
 -- Lua setup
 lspconfig.lua_ls.setup {
